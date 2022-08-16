bb1ad153a104443a2a9b593ca3980401c71a6557
Jaakko Saarinen <jsaarine@student.hive.fi>
2022-08-16 19:14:46 +0300

    remove libsdl2

--- libsdl2/include/SDL_revision.h
+++
@@ -1,2 +0,0 @@
-#define SDL_REVISION "git@vogsphere-v2.hive.fi:vogsphere/intra-uuid-385d55fd-ad07-41db-b0aa-f6f1cca4a428-4262611-jsaarine@bae8ce8325af3514328de5d1cda32504a735f576"
-#define SDL_REVISION_NUMBER 0
