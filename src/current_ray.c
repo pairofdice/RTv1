ef82da9f365bb95922142615b99629d5833f7f7a
Jaakko Saarinen <jsaarine@student.hive.fi>
2022-09-26 16:50:25 +0300

    norm camera, current_ray

--- src/current_ray.c
+++ src/current_ray.c
@@ -6,7 +6,7 @@
 /*   By: jsaarine <jsaarine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 15:22:54 by jsaarine          #+#    #+#             */
-/*   Updated: 2022/09/13 15:24:23 by jsaarine         ###   ########.fr       */
+/*   Updated: 2022/09/26 16:48:28 by jsaarine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 void	calc_current_ray(t_context *ctx, int x, int y)
 {
-	ctx->ray.orig = vec3_add(ctx->cam.L, vec3_scalar_mult(ctx->cam.right, x * ctx->cam.projection_plane_w / WIN_W));
-	ctx->ray.orig = vec3_add(ctx->ray.orig, vec3_scalar_mult(ctx->cam.up, y * ctx->cam.projection_plane_h / WIN_H));
+	ctx->ray.orig = vec3_add(ctx->cam.L, vec3_scalar_mult(ctx->cam.right,
+				x * ctx->cam.projection_plane_w / WIN_W));
+	ctx->ray.orig = vec3_add(ctx->ray.orig, vec3_scalar_mult(ctx->cam.up,
+				y * ctx->cam.projection_plane_h / WIN_H));
 	ctx->ray.dir = vec3_sub(ctx->ray.orig, ctx->cam.loc);
 	ctx->ray.dir = vec3_unit(ctx->ray.dir);
 }
